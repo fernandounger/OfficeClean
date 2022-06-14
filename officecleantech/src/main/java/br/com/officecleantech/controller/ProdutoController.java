a8fb0684b816e88193c8e86152da76733c2743a9
Isaque Fernandes <isaquefernandesuser@outlook.com>
2022-06-14 20:02:54 -0300

    " REFACTOR: [Deleção da página fornecedores,produtos e suas páginas subsequentes] "

--- officecleantech/src/main/java/br/com/officecleantech/controller/ProdutoController.java
+++
@@ -1,5 +0,0 @@
-package br.com.officecleantech.controller;
-
-public class ProdutoController {
-
-}
--- officecleantech/src/main/java/br/com/officecleantech/model/dao/ControleEntradaDao.java
+++
@@ -1,5 +0,0 @@
-package br.com.officecleantech.model.dao;
-
-public class ControleEntradaDao {
-
-}
